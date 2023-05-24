0e76d26e31aa02107364f444f0a5b9cdaac8d3d8
Kevin Muscara <muscarak1@nku.edu>
2023-05-24 17:50:16 -0400

    first users route with prisma

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
 
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+app.set('views', require('path').join(__dirname, 'views'))
+app.set('view engine', 'ejs');
+
 app.get("/", async(req, res) => {
-  res.send({ status: 200, message: 'hello world!' });
+  res.render('index.ejs', { message: `hello world!` });
 });
 
-app.listen(process.env.PORT, process.env.HOST, () => console.log(`live`));
+app.get("/users", async(req, res) => {
+  let users = await prisma.user.findMany();
+  res.send({ status: 200, users });
+});
+
+app.listen(process.env.PORT || 80, process.env.HOST || '0.0.0.0', () => console.log(`live`));
