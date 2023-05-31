4db762771cbf93ac56a6614572b14c1e43298d82
Kevin Muscara <muscarak1@nku.edu>
2023-05-30 23:21:27 -0400

    updated orm functions

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,8 +1,14 @@
-const { PrismaClient } = require('@prisma/client');
 const express = require('express');
 const router = express.Router();
-const prisma = new PrismaClient();
+const { getUsers, getUser } = require('../orm/read/user');
 
-router.get('/', async(req, res) => res.send({ status: 200, users: await prisma.user.findMany() }));
+router.get('/', async(req, res) => {
+  res.send({ status: 200, users: await getUsers() });
+});
+
+router.get('/:id', async(req, res) => {
+  let user = await getUser(req.params.id);
+  res.send({ status: user == null ? 404 : 200, user: user == null ? [] : user });
+})
 
 module.exports = router;
--- src/routes/util/auth.js
+++
@@ -1,9 +0,0 @@
-exports.checkPerm = function(req, res, next) {
-  // future authorization
-  if(true) {
-    next();
-  } else {
-    res.send({ status: 403, message: `Forbidden` });
-    return;
-  }
-};
